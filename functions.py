4c40cb6f3285c05729a7818237eb2d6785b1ab3a
achilimoniuk <101393635+achilimoniuk@users.noreply.github.com>
2023-06-09 20:12:18 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# creating random dates
+def random_dates(start, end, n, unit='D', seed=None):
+    if not seed:  # from piR's answer
+        np.random.seed(0)
+    ndays = (end - start).days + 1
+    return pd.to_timedelta(np.random.rand(n) * ndays, unit=unit) + start
+
